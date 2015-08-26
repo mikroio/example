102db9982bf95f7b29fc43e7c188431b5142e47e
Johan Rydberg <johan.rydberg@gmail.com>
2015-08-26 21:12:50 +0200

    cruel cold world

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-     io.WriteString(w, "Hello cruel world!")
+     io.WriteString(w, "Hello cruel cold world!")
 }
 
 func main() {
